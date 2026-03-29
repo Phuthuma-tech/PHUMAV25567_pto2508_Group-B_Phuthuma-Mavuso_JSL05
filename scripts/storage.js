ab837cfdcdb68c5bedd8f049d1ddd838330ec345
Phuthuma-tech <101583880+Phuthuma-tech@users.noreply.github.com>
2026-03-29 12:27:00 +0200

    Create storage.js
    Create an initial storage file that allows the function to work properly

---
+++ scripts/storage.js
@@ -0,0 +1,16 @@
+/**
+ * Retrieves tasks from localStorage.
+ * @returns {Array} An array of task objects.
+ */
+export const getTasks = () => {
+    const tasks = localStorage.getItem('tasks');
+    return tasks ? JSON.parse(tasks) : [];
+};
+
+/**
+ * Saves the task array to localStorage.
+ * @param {Array} tasks - The current list of tasks.
+ */
+export const saveTasks = (tasks) => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+};
